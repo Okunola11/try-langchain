13ad2ad807a9c4fa0d7ae9198d62426112c421e3
okunola <abdayoade@gmail.com>
2024-08-30 21:21:03 +0100

    chore: update settings.py
    Changed the environment name of OPEN_API_KEY to match the value in the .env file.

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ from pathlib import Path
 env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
-OPEN_API_KEY = os.environ.get("OPEN_API_KEY")
+OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
 UPSTASH_VECTOR_REST_TOKEN = os.environ.get("UPSTASH_VECTOR_REST_TOKEN")
 UPSTASH_VECTOR_REST_URL = os.environ.get("UPSTASH_VECTOR_REST_URL")
