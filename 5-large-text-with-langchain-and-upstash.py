b8fffbce7275f6dea59f326ef37d2a5cdd5eb8b4
okunola <abdayoade@gmail.com>
2024-08-30 08:59:44 +0100

    chore: add large vector data to the upstash index
    Data about cities fetched from wikipedia was turned into embeddings,
    split into token chunks of 100 per row and saved to the upstash index
    database.

---
+++ 5-large-text-with-langchain-and-upstash.py
@@ -0,0 +1,53 @@
+import os
+import wikipedia
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import UpstashVectorStore
+from langchain_core.documents import Document
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import TokenTextSplitter
+
+from settings import OPEN_API_KEY, UPSTASH_VECTOR_REST_TOKEN, UPSTASH_VECTOR_REST_URL
+
+os.environ['OPENAI_API_KEY'] = OPEN_API_KEY
+
+ny = wikipedia.page(title="New York City, New York")
+# print(ny.content)
+
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
+
+store = UpstashVectorStore(
+    embedding=embeddings,
+    index_url=UPSTASH_VECTOR_REST_URL,
+    index_token=UPSTASH_VECTOR_REST_TOKEN
+)
+
+documents = []
+cities = ["New York City, New York", "Lagos Island, Lagos"]
+for city in cities:
+    wikipedia_page_result = wikipedia.page(title=city)
+    doc = Document(
+        page_content=wikipedia_page_result.content,
+        metadata={
+            "source": f"{wikipedia_page_result.url}",
+            "title": city
+        }
+    )
+    documents.append(doc)
+
+print(documents[0].metadata)
+print(len(documents))
+
+OPENAI_LLM_MODEL = "gpt-4o"
+text_splitter = TokenTextSplitter.from_tiktoken_encoder(
+    model_name=OPENAI_LLM_MODEL, chunk_size=100, chunk_overlap=0
+)
+
+docs = text_splitter.split_documents(documents)
+print(len(docs))
+
+# inserted_vectors = store.add_documents(docs)
+
+result = store.similarity_search_with_score("Obalende", k=2)
+# print(result)
+for doc, score in result:
+    print(f"{doc.metadata} - {score}")
