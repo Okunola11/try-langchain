110483c2f8149de4cbac15c674ec81386e1957df
okunola <abdayoade@gmail.com>
2024-08-31 09:06:11 +0100

    chore: update settings.py
    Added new environment variables for redis configuration and api access
    key.

--- settings.py
+++ settings.py
@@ -6,5 +6,12 @@ env_path = Path(".") / ".env"
 load_dotenv(dotenv_path=env_path)
 
 OPEN_API_KEY = os.environ.get("OPENAI_API_KEY")
+
 UPSTASH_VECTOR_REST_TOKEN = os.environ.get("UPSTASH_VECTOR_REST_TOKEN")
 UPSTASH_VECTOR_REST_URL = os.environ.get("UPSTASH_VECTOR_REST_URL")
+
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = os.environ.get("REDIS_PORT", 6379)
+REDIS_PASSWORD = os.environ.get("REDIS_PASSWORD")
+
+API_ACCESS_KEY = os.environ.get("API_ACCESS_KEY")
